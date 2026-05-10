63382d26f739ae2b973ccb9fd11bed44530f0c2c
user <user@debian-13.3.myguest.virtualbox.org>
2026-05-10 20:12:34 +0400

    added logging to ghcr

--- run_app.sh
+++ run_app.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 SHA=$1
 IMAGE=$2
+GITHUBTOKEN=$3
+GITHUBACTOR=$4
 CONTAINER_NAME="catty-container"
+
+echo "Logging in GHCR"
+echo "$GITHUBTOKEN" | docker login ghcr.io -u $GITHUBACTOR --password-stdin
+
 echo "Pulling image: $IMAGE"
 docker pull $IMAGE
 docker stop $CONTAINER_NAME || true
